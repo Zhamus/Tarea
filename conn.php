eab0118a97df62d476d3c5614c24aa9b1d99936c
Zhamus <al221810713@gmail.com>
2021-10-30 13:40:45 -0500

    Trabajo

--- conn.php
+++ conn.php
@@ -1,8 +1,14 @@
 <?php
-	$db_username = 'roman';
+	/*$db_username = 'roman';
 	$db_password = 'soyelnery12';
 	$conn = new PDO( 'mysql:host=18.191.125.118;dbname=db_pdo_crud', $db_username, $db_password );
 	if(!$conn){
 		die("Fatal Error: Connection Failed!");
-	}
+	}*/
+	$conn = mysqli_connect(
+		'18.191.125.118',
+		'roman',
+		'soyelnery12',
+		'db_pdo_crud',
+	  ) or die(mysqli_erro($mysqli));
 ?>
